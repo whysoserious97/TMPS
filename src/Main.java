80029ee369492b1db38930853a6ad44305bfc7e7
whysoserious <wildwhysoserious@gmail.com>
2020-10-22 13:25:27 +0300

    "Checkpoint" - with fails.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import StructuralDP.SuperDecorator;
 import System.Users.User;
 import System.ITSystem;
 public class Main {
@@ -18,14 +17,14 @@ public class Main {
         User user3=system.getNewUser("internal","Client");
 
         //Trying to loggin for existing ones
-        System.out.println(user1.login("guest","guest"));
+        System.out.println(user1.login());
         System.out.println(user1.getUserType());
-        System.out.println(user2.login("guest","guest"));
+        System.out.println(user2.login());
         System.out.println(user2.getUserType());
 
         // Because 3rd is not valid he should be null and an exception has been risen.
         try {
-            System.out.println(user3.login("guest","guest"));
+            System.out.println(user3.login());
             System.out.println(user3.getUserType());
         }catch (Exception e){
             System.out.println("3rd user could not login");
@@ -34,16 +33,5 @@ public class Main {
         // 2nd time we should get the same instance of System.ITSystem
         system= ITSystem.getITSystem();
         System.out.println("Second system:"+system.hashCode());
-
-
-
-        /*
-         Structural paterns
-
-         */
-        // Decorator test - we want to create a super user that will be able to do everything
-        User superUser = new SuperDecorator(user1);
-        System.out.println(superUser.getUserType());
-        System.out.println(superUser.login("admin","admin"));
     }
 }
--- src/StructuralDP/PerimisionSet.java
+++
@@ -1,7 +0,0 @@
-package StructuralDP;
-
-/*
-This class is used to mark a set of permisions that a user will have
- */
-public class PerimisionSet {
-}
